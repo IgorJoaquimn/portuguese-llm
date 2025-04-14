7971addc6730983f331a2b47f6dbf24f0bf2fc2a
IgorJoaquimn <igor-joaquim@ufmg.br>
2025-04-14 07:15:59 -0300

    masked masked_personas

--- src/masked/masked_personas.py
+++ src/masked/masked_personas.py
@@ -14,22 +14,37 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 logging.getLogger().setLevel(logging.ERROR)
 
+avoid_zero_division = 1e-10
+
 def get_count_vector(corpus,cv):
     """
     Get the count vector of the corpus
     """
     count_vector = cv.fit_transform(corpus)
-    return count_vector
+    return np.sum(count_vector, axis=0) 
+
+def generate_vocab(corpus, ngrams=2):
+    """
+    Generate the vocabulary of the corpus
+    """
+    cv  = CountVectorizer(
+            analyzer='word',
+            ngram_range=(1, ngrams)
+    )
+    # Generate the count vectors
+    count_vector = cv.fit_transform(corpus)
+    vocab = cv.get_feature_names_out()
+    return vocab
 
 def get_log_odds(target_words, unmarked_words, prior_words, ngrams=2):
-    vocab = set(target_words + unmarked_words + prior_words)
+    vocab = generate_vocab([target_words,unmarked_words,prior_words], ngrams=ngrams)
     cv  = CountVectorizer(
             vocabulary=vocab,
+            analyzer='word',
             decode_error='ignore',
             min_df=10,
             max_df=0.8, # Ignore words that appear in more than 80% of the documents
-            ngrams=(1, ngrams), 
-            binarize=False,
+            ngram_range=(1, ngrams),
             max_features=10000,
     )
     # Generate the count vectors
@@ -40,9 +55,14 @@ def get_log_odds(target_words, unmarked_words, prior_words, ngrams=2):
     assert prior.shape == target.shape == unmarked.shape, "The shape of the count vectors must be the same"
 
     # Get the sum over all ngrams
-    prior_total = np.sum(prior, axis=0)
-    target_total = np.sum(target, axis=0)
-    unmarked_total = np.sum(unmarked, axis=0)    
+    prior_total = np.sum(prior,axis=1).reshape(1,-1)
+    target_total = np.sum(target,axis=1).reshape(1,-1)
+    unmarked_total = np.sum(unmarked,axis=1).reshape(1,-1)
+
+    assert prior_total.shape == target_total.shape == unmarked_total.shape, "The shape of the count vectors must be the same"
+    assert prior_total.shape == (1,1), "The shape of the count vectors must be 1"
+    assert target_total.shape == (1,1), "The shape of the count vectors must be 1"
+    assert unmarked_total.shape == (1,1), "The shape of the count vectors must be 1"
 
     new_vocab = cv.vocabulary
     vocab_size = len(new_vocab)
@@ -55,10 +75,11 @@ def get_log_odds(target_words, unmarked_words, prior_words, ngrams=2):
 
     delta = target_term - unmarked_term
     
-    var = 1./(target + prior) + 1./(unmarked + prior)
+    var = 1./(target + prior + avoid_zero_division) + 1./(unmarked + prior + avoid_zero_division) # Avoid division by zero
     z_scores = delta / np.sqrt(var)
     # Return dict<str, float> the ngram and the z_score
-    z_scores_dict = {k: v for k, v in zip(cv.get_features_names_out(), z_scores)}
+    print(z_scores)
+    z_scores_dict = {k: v for k, v in zip(cv.get_feature_names_out(), z_scores.flatten())}
     return z_scores_dict
 
 
@@ -68,4 +89,11 @@ def masked_personas(record,target_group, unmarked_group,ngrams=2):
     
 
 if __name__ == '__main__':
-    pass
+    # Example usage
+    target_words   = ["one", "two", "three", "one", "two", "three", "ten"].join(" ")
+    unmarked_words = ["one", "two", "three", "one", "two", "three", "five","one"].join(" ")
+    prior_words = ["this", "is"].join(" ")
+
+
+    z_scores_dict = get_log_odds(target_words, unmarked_words, prior_words, ngrams=3)
+    print(z_scores_dict)
